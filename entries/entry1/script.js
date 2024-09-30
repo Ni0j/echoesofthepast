ece3c185a1ab6a59ca6482504dae1fa8a74ae9d5
Ni0j <jinj952@newschool.edu>
2024-09-30 19:38:51 -0400

    1

--- entries/entry1/script.js
+++ entries/entry1/script.js
@@ -7,11 +7,10 @@ const body = document.querySelector('body');
 document.querySelector("body > nav > div.nav-wrapper > div > button")
 
 const scrollElement = document.getElementById('scroll');
-const bodyElement = document.body;
 const quoteElement = document.getElementById('quote');
 const textContainer = document.querySelector('.text-container');
 
-let isQuoteVisible = false; 
+let isQuoteVisible = false;
 
 window.addEventListener('scroll', function() {
     const rect = scrollElement.getBoundingClientRect();
@@ -19,24 +18,19 @@ window.addEventListener('scroll', function() {
     // 检查quote是否在视口内
     if (rect.top <= window.innerHeight && rect.bottom >= 0) {
         if (!isQuoteVisible) {
-
             isQuoteVisible = true;
-
-            // 应用效果
-            document.body.style.backgroundColor = 'black'; // 背景变为黑色
-            quoteElement.classList.add('blur'); // 添加模糊效果
-            textContainer.classList.add('blur'); // 添加模糊效果到其他文本
+            
+            // 应用背景颜色和模糊效果
+            document.body.classList.add('fade-in');
+            textContainer.classList.add('blur');
         }
-
-        // 确保页面固定在quote块
-        window.scrollTo(0, rect.top + window.scrollY);
     } else {
         if (isQuoteVisible) {
-            // 用户离开quote块，恢复效果
             isQuoteVisible = false;
-            document.body.style.backgroundColor = '#F7F7F7'; // 恢复背景颜色
-            quoteElement.classList.remove('blur'); // 移除模糊效果
-            textContainer.classList.remove('blur'); // 移除模糊效果
+            
+            // 恢复背景颜色和模糊效果
+            document.body.classList.remove('fade-in');
+            textContainer.classList.remove('blur');
         }
     }
 });
