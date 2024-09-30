ab62ef3238baeb74c5c3514f4ba45ed704f5bcd0
Ni0j <jinj952@newschool.edu>
2024-09-29 22:37:56 -0400

    initial

---
+++ entries/script.js
@@ -0,0 +1,13 @@
+
+
+document.addEventListener("DOMContentLoaded", function(){
+const body = document.querySelector('body');
+
+
+document.querySelector("body > nav > div.nav-wrapper > div > button")
+
+
+
+})
+
+
