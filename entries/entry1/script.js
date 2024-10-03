88c556cf50799a3dfb0cff18cc09955eaa896a4b
Ni0j <jinj952@newschool.edu>
2024-10-03 19:35:02 -0400

    subtitle margin

--- entries/entry1/script.js
+++ entries/entry1/script.js
@@ -6,34 +6,34 @@ const body = document.querySelector('body');
 
 document.querySelector("body > nav > div.nav-wrapper > div > button")
 
-const scrollElement = document.getElementById('scroll');
-const quoteElement = document.getElementById('quote');
-const textContainer = document.querySelector('.text-container');
+// const scrollElement = document.getElementById('scroll');
+// const quoteElement = document.getElementById('quote');
+// const textContainer = document.querySelector('.text-container');
 
-let isQuoteVisible = false;
+// let isQuoteVisible = false;
 
-window.addEventListener('scroll', function() {
-    const rect = scrollElement.getBoundingClientRect();
+// window.addEventListener('scroll', function() {
+//     const rect = scrollElement.getBoundingClientRect();
 
-    // 检查quote是否在视口内
-    if (rect.top <= window.innerHeight && rect.bottom >= 0) {
-        if (!isQuoteVisible) {
-            isQuoteVisible = true;
+//     // 检查quote是否在视口内
+//     if (rect.top <= window.innerHeight && rect.bottom >= 0) {
+//         if (!isQuoteVisible) {
+//             isQuoteVisible = true;
             
-            // 应用背景颜色和模糊效果
-            document.body.classList.add('fade-in');
-            textContainer.classList.add('blur');
-        }
-    } else {
-        if (isQuoteVisible) {
-            isQuoteVisible = false;
+//             // 应用背景颜色和模糊效果
+//             document.body.classList.add('fade-in');
+//             textContainer.classList.add('blur');
+//         }
+//     } else {
+//         if (isQuoteVisible) {
+//             isQuoteVisible = false;
             
-            // 恢复背景颜色和模糊效果
-            document.body.classList.remove('fade-in');
-            textContainer.classList.remove('blur');
-        }
-    }
-});
+//             // 恢复背景颜色和模糊效果
+//             document.body.classList.remove('fade-in');
+//             textContainer.classList.remove('blur');
+//         }
+//     }
+// });
 
 
 const toggleButton = document.getElementById('toggleLanguage');
