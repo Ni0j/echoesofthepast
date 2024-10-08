404d3969d33a82faa7c21b9eedf569d984b69e91
Ni0j <jinj952@newschool.edu>
2024-10-08 01:45:03 -0400

    about

---
+++ entries/aboutEN/script.js
@@ -0,0 +1,64 @@
+
+
+document.addEventListener("DOMContentLoaded", function(){
+const body = document.querySelector('body');
+
+
+document.querySelector("body > nav > div.nav-wrapper > div > button")
+
+// const scrollElement = document.getElementById('scroll');
+// const quoteElement = document.getElementById('quote');
+// const textContainer = document.querySelector('.text-container');
+
+// let isQuoteVisible = false;
+
+// window.addEventListener('scroll', function() {
+//     const rect = scrollElement.getBoundingClientRect();
+
+//     // 检查quote是否在视口内
+//     if (rect.top <= window.innerHeight && rect.bottom >= 0) {
+//         if (!isQuoteVisible) {
+//             isQuoteVisible = true;
+            
+//             // 应用背景颜色和模糊效果
+//             document.body.classList.add('fade-in');
+//             textContainer.classList.add('blur');
+//         }
+//     } else {
+//         if (isQuoteVisible) {
+//             isQuoteVisible = false;
+            
+//             // 恢复背景颜色和模糊效果
+//             document.body.classList.remove('fade-in');
+//             textContainer.classList.remove('blur');
+//         }
+//     }
+// });
+
+
+const toggleButton = document.getElementById('toggleLanguage');
+const chineseText = document.getElementById('chinese-text');
+const englishText = document.getElementById('english-text');
+
+// 默认中文显示，英文隐藏
+chineseText.style.display = 'block';
+englishText.style.display = 'none';
+
+toggleButton.addEventListener('click', function() {
+  if (chineseText.style.display === 'none') {
+    chineseText.style.display = 'block';
+    englishText.style.display = 'none';
+    toggleButton.textContent = 'English';
+  } else {
+    chineseText.style.display = 'none';
+    englishText.style.display = 'block';
+    toggleButton.textContent = '中文';
+  }
+});
+
+
+
+
+})
+
+
